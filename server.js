288757cd53bf48aad84103c3abd55b4e77b304b6
UP210305 <up210305@alumnos.upa.edu.mx>
2023-02-14 04:09:12 -0600

    Port 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { createReadStream } = require('fs');
 const http = require('http');
 
-const port = 8080;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
