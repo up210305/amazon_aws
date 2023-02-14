325bb7b19207bfdf1c5b1ade1c7e60c7a047aae8
UP210305 <up210305@alumnos.upa.edu.mx>
2023-02-14 04:37:28 -0600

    Server

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
+const http = require('http');
+const fs = require('fs').promises;
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  //res.end(' Hello World ' );
+  fs.readFile(__dirname + "/index.html")
         .then(contents => {
             res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
@@ -10,4 +18,9 @@ const requestListener = function (req, res) {
             res.end(err);
             return;
         });
-};
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
--- server.js
+++ server.js
@@ -1,23 +1,12 @@
+const { createReadStream } = require('fs');
 const http = require('http');
-const fs = require('fs').promises;
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  //res.end(' Hello World ' );
-  fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
+  res.end(' Hello World ' );
 });
 
 server.listen(port, () => {
